1c449039b2c360580e023d6dcdb2b829ab7186b2
dkhara <darius_kharazi@anfcorp.com>
2020-11-17 21:56:25 -0500

    Add meta data

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Header from './header'
 import Footer from './footer'
+import { Helmet } from "react-helmet"
 
 const Layout = ({ children }) => {
     return (
         <div className="wrap">
+            <Helmet>
+                <meta name="description" content="Personal Site" />
+                <meta name="title" property="og:title" content="Data Science" />
+                <meta property="og:type" content="Website" />
+                <meta name="image" property="og:image" content="https://live.staticflickr.com/8601/15891138064_3a9f462786_k.jpg" />
+                <meta name="description" property="og:description" content="Personal Site" />
+                <meta name="author" content="Darius Kharazi" />
+            </Helmet>
             <Header />
             <main className="container">
                 {children}
